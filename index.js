4ddd537972942286469a0dc22a9c97897b5625d6
NabigElraih <87423295+NabigElraih@users.noreply.github.com>
2024-12-18 17:07:03 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req,res)=>{
-res.send('
-         <h1>Hello World this Nabig from JS app test1</>
-');
+res.send("
+hello World, Nabig");
 });
 
 app.listen(5000, console.log("server is listening on port 5000"));
